e48fccc70c2da53efccf53e3536ecf9754dff274
sheerazAlvi <m.sheerazAlvi@gmail.com>
2018-11-04 17:32:53 -0500

    First chapter programming practice.

---
+++ KickStart/src/HelloWorld.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 4, 2018
+
+@author: hp
+'''
+print('Hello World!\n');
+totalTimeSeconds = 42 + (60 * 42);
+totalTimeMinutes = totalTimeSeconds / 60;
+print('Total Time in Minutes =', totalTimeMinutes);
+miles = 10/1.61;
+print('Given Miles =', miles);
+MilesPerMinute = miles/totalTimeMinutes;
+print('Miles per minute =', MilesPerMinute);
+print('Miles per hour =', MilesPerMinute*60);
